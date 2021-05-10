20b35ee607fabac4489f6df02944d7c486aeb67a
mihyeon <2_5_3_1@naver.com>
2021-05-10 23:36:48 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_socket/src/View/loading.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Up Down Game',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Loading(),
+    );
+  }
+}
